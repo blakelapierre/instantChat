89aca024a99321d3d3c3f464e50f2c5cae0a4a29
Blake La Pierre <blakelapierre@gmail.com>
2014-06-30 13:27:44 -0700

    No echo cancellation? We'll just mute local audio stream for now.

--- site/src/frontend/directives/stream/directive.js
+++ site/src/frontend/directives/stream/directive.js
@@ -2,8 +2,15 @@ module.exports = () => {
   return {
     restrict: 'E',
     template: require('./template.html'),
+    scope: {
+      stream: '='
+    },
     link: ($scope, element, attributes) => {
+      var video = element.find('video')[0];
 
+      $scope.$watch('stream', stream => {
+        video.muted = stream.isLocal;
+      });
     },
     controller: ['$scope', ($scope) => {
 
