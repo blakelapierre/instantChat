5f22097fd16d90d4ec9142309f76866340e7281f
Blake La Pierre <blakelapierre@gmail.com>
2014-07-05 14:49:15 -0700

    I'd like to get traceur on the server side code to clean this up more...

--- site/src/server/signal.js
+++ site/src/server/signal.js
@@ -93,60 +93,13 @@ function HashList(idProperty) {
   };
 
   this.forEach = function(fn) {
-    for (var i = 0; i < list.length; i++) {
-      fn(list[i]);
-    }
+    for (var i = 0; i < list.length; i++) fn(list[i]);
   };
 }
 
 module.exports = function(io) {
   var rooms = new HashList('_roomName'),
       sockets = new HashList('id');
-
-
-  function joinRoom(socket, roomName) {
-    var room = rooms.getByID(roomName);
-
-    if (room === null) {
-      room = new HashList('id');
-      room._roomName = roomName;
-      rooms.push(room);
-    }
-
-    room.forEach(function(peerSocket) {
-      peerSocket.emit('peer join', socket.id);
-    });
-
-    socket.emit('peer list', {
-      roomName: roomName,
-      peerIDs: _.pluck(room.asList(), 'id')
-    });
-
-    room.push(socket);
-
-    socket.rooms.push(roomName);
-    console.log('join', roomName, socket.id);
-  }
-
-  function leaveRoom(socket, roomName) {
-    console.log('leave', roomName, socket.id);
-    var room = rooms.getByID(roomName);
-
-    if (room === null) {
-      console.log('Tried to leave non-existent room', roomName);
-      return;
-    }
-
-    room.removeObject(socket);
-
-    socket.rooms.removeByID(roomName);
-
-    room.forEach(function(peerSocket) {
-      peerSocket.emit('peer leave', socket.id);
-    });
-
-    if (room.length() === 0) rooms.removeByID(roomName);
-  }
 
   io.sockets.on('connection', function(socket) {
     socket.emit('your_id', socket.id);
@@ -189,23 +142,58 @@ module.exports = function(io) {
       }
     });
 
-    socket.on('room join', function(roomName) {
-      joinRoom(socket, roomName);
-    });
+    socket.on('room join', function(roomName) { joinRoom(socket, roomName); });
 
-    socket.on('room leave', function(roomName) {
-      leaveRoom(socket, roomName);
-    });
+    socket.on('room leave', function(roomName) { leaveRoom(socket, roomName); });
 
     socket.on('disconnect', function() {
       console.log('disconnect', socket.id);
-      socket.rooms.forEach(function(roomName) {
-        leaveRoom(socket, roomName);
-      });
+
+      socket.rooms.forEach(function(roomName) { leaveRoom(socket, roomName); });
 
       sockets.removeObject(socket);
     });
   });
+
+  function joinRoom(socket, roomName) {
+    var room = rooms.getByID(roomName);
+
+    if (room === null) {
+      room = new HashList('id');
+      room._roomName = roomName;
+      rooms.push(room);
+    }
+
+    room.forEach(function(peerSocket) { peerSocket.emit('peer join', socket.id); });
+
+    socket.emit('peer list', {
+      roomName: roomName,
+      peerIDs: _.pluck(room.asList(), 'id')
+    });
+
+    room.push(socket);
+
+    socket.rooms.push(roomName);
+    console.log('join', roomName, socket.id);
+  }
+
+  function leaveRoom(socket, roomName) {
+    console.log('leave', roomName, socket.id);
+    var room = rooms.getByID(roomName);
+
+    if (room === null) {
+      console.log('Tried to leave non-existent room', roomName);
+      return;
+    }
+
+    room.removeObject(socket);
+
+    socket.rooms.removeByID(roomName);
+
+    room.forEach(function(peerSocket) { peerSocket.emit('peer leave', socket.id); });
+
+    if (room.length() === 0) rooms.removeByID(roomName);
+  }
 
   return {
     rooms: rooms,
