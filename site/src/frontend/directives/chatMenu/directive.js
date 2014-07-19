0f6015803cfe599681b54c000db3bd6aaa02a550
Blake La Pierre <blakelapierre@gmail.com>
2014-07-18 17:36:19 -0700

    New distribution channels.

--- site/src/frontend/directives/chatMenu/directive.js
+++ site/src/frontend/directives/chatMenu/directive.js
@@ -14,6 +14,8 @@ module.exports = () => {
         emailSubject: 'I want to chat with you!',
         emailBody() { return 'Come join me at ' + encodeURIComponent(window.location.toString()); },
         smsBody() { return 'Come join me at ' + encodeURIComponent(window.location.toString()); },
+        inviteLink() { return encodeURIComponent(window.location.toString()); },
+        inviteText: 'I\'m currently video chatting. Come join me!',
 
 
         isMouseInside: true,
