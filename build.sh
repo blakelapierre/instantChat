764322ccbdc72a4f6424fed55d2e02d4f0f9e410
Blake La Pierre <blakelapierre@gmail.com>
2014-09-15 01:48:17 -0700

    .

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit on first error
+set -e
+
+# Mount rount
+sudo mount -t tmpfs none /root
+
+# Kill background processes on exit
+sudo trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
+
+# Workaround with cgroup for recent version of docker
+sudo cgroups-umount
+sudo cgroups-mount
+
+# Start docker daemon
+sudo docker -d -H 0.0.0.0:4243 -H unix:///var/run/docker.sock 2>> /dev/null >> /dev/null &
+sleep 2
+docker build services/broadcaster
